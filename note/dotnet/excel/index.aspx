efa677dbd216c3c10e547bb88821edc088201c62
wellfrog <wellfrog16@gmail.com>
2017-05-15 19:55:40 +0800

    日常

---
+++ note/dotnet/excel/index.aspx
@@ -0,0 +1,13 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeFile="index.aspx.cs" Inherits="dotnet_excel_index" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8"/>
+    <title></title>
+</head>
+<body>
+    <asp:Literal runat="server" ID="result"></asp:Literal>
+</body>
+</html>
---
+++ note/dotnet/excel/index.aspx.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.OleDb;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+public partial class dotnet_excel_index : System.Web.UI.Page
+{
+    protected void Page_Load(object sender, EventArgs e)
+    {
+        DataSet ds = ExcelToDS(Server.MapPath("suppiler.xlsx"));
+
+        DataView dv = ds.Tables[0].DefaultView;
+
+
+        List<string> list = new List<string>();
+
+        var s = "";
+
+        for(var i = 0; i < dv.Count; i++)
+        {
+            s += "{\"province\":\"" + dv[i][3].ToString().Replace("省", "").Replace("壮族自治区", "").Replace("维吾尔自治区", "").Replace("自治区", "") + "\"";
+            s += ",\"city\":\"" + dv[i][4].ToString().Replace("市", "") + "\"";
+            s += ",\"company\":\"" + dv[i][6].ToString() + "\"";
+            s += ",\"address\":\"" + dv[i][7].ToString() + "\"";
+            s += ",\"mobile\":\"" + dv[i][9].ToString() + "\"";
+            s += ",\"tel\":\"" + dv[i][10].ToString() + "\"";
+            s += "}";
+
+            list.Add(s);
+            s = "";
+        }
+
+        result.Text = "[" + String.Join(",", list.ToArray()) + "]";
+    }
+
+    public DataSet ExcelToDS(string Path)
+    {
+        //string strConn = "Provider=Microsoft.Jet.OLEDB.4.0;" + "Data Source=" + Path + ";" + "Extended Properties=Excel 8.0;";
+        string strConn = "Provider=Microsoft.Ace.OleDb.12.0;" + "data source=" + Path + ";Extended Properties='Excel 12.0; HDR=no; IMEX=1'";
+        OleDbConnection conn = new OleDbConnection(strConn);
+        conn.Open();
+        string strExcel = "";
+        OleDbDataAdapter myCommand = null;
+        DataSet ds = null;
+        strExcel = "select * from [sheet1$]";
+        myCommand = new OleDbDataAdapter(strExcel, strConn);
+        ds = new DataSet();
+        myCommand.Fill(ds, "table1");
+        conn.Close();
+        return ds;
+    }
+}
